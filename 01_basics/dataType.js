8507d0384f0b6d86b45d3d5091a762e4d01ca102
Ayush-sis0diya <xerbias2026@gmail.com>
2025-08-27 10:54:32 +0000

    datatype

---
+++ 01_basics/dataType.js
@@ -0,0 +1,12 @@
+let name = "ayush " // string data type
+let age = 18; // int data type
+let islogggin = true // bollean data type = true/false
+// null = standalone value;
+// undefined
+//symbol => is it used to find the uniqueness
+
+
+// object 
+
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
