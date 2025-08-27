621d7fcfbf157bf2c5cb1606217b52ecdd95c482
Ayush-sis0diya <xerbias2026@gmail.com>
2025-08-27 10:29:48 +0000

    varriable

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const name = "ayush";
+let email = "ayush1088@gMIL.COM";
+var number = "1122233";
+
+console.log(name, email, number);
+// ayush ayush1088@gMIL.COM 1122233
+
+console.table([name, email, number]);
+/*
+┌─────────┬──────────────────────┐
+│ (index) │ Values               │
+├─────────┼──────────────────────┤
+│ 0       │ 'ayush'              │
+│ 1       │ 'ayush1088@gMIL.COM' │
+│ 2       │ '1122233'            │
+└─────────┴──────────────────────┘
+*/
