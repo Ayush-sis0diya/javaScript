4ad76521addb52d53ffe9209b52e08fd729dd032
Ayush-sis0diya <xerbias2026@gmail.com>
2025-08-27 22:41:47 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,43 @@
+let score = true;
+// console.log(typeof score);
+
+let newVar2 = Number(score);
+// console.log(newVar2);
+
+let ABC = "ayush";
+let newVar = Number(ABC);
+// console.log(newVar);
+
+// number to boolean 
+
+let isLoggedin = 1;
+let booleanLoggedin = Boolean(isLoggedin);
+// console.log(booleanLoggedin);
+
+
+
+let isLoggedin1 = " ";
+//let booleanLoggedin1= Boolean(isLoggedin);
+// console.log(booleanLoggedin1);
+
+
+//********************************OPERATION********************************************/
+
+// console.log(1+2);
+// console.log(1*2);
+// console.log(1-2);
+// console.log(1**2);
+// console.log(1/2);
+// console.log(1%2);
+
+//string + number
+
+// console.log("1" + 2);  //1+2 = 12;
+// console.log(1+2*3+"5"); // 1+2*3= 7 => 7+5 = 75;
+
+// console.log("a" + 2);  //a2
+// console.log(1+2*3+"2AA") //72AA
+
+console.log(+true) // 1
+// console.log(true+) // error dega;
+
