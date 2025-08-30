4258ac5378b6d1d4ba14ae33238279f506285af3
Ayush-sis0diya <xerbias2026@gmail.com>
2025-08-30 10:30:47 +0000

    number_and_Math

---
+++ 01_basics/nums_math.js
@@ -0,0 +1,33 @@
+const score    = 400;
+
+const balance = new Number(100);
+
+// console.log(score);
+// console.log(balance)
+
+// console.log(balance.toFixed(3));
+
+const number = 123.1189;
+// console.log(number.toPrecision(4));
+
+
+
+//**********************Maths************************ */
+
+
+// console.log(Math.abs(-2)); //2
+/////// Math.random/////
+// console.log(Math.random()); //  0 t0 1    ,  0 included but 1 not
+
+// console.log(Math.floor(Math.random()*10));  //7 random variable btw[0-9]
+
+// console.log(Math.floor(Math.random()*10 +1)); // [1-10]
+
+
+
+// ************************************Date*************************************** //
+const date = new Date(2025 ,0 ,16)
+console.log(date.toDateString()); // Thu Jan 16 2025
+
+
+
