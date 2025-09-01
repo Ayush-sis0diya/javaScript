35868fcc5fe698553fedb1344b0c2335cfd5a79a
Ayush-sis0diya <xerbias2026@gmail.com>
2025-09-01 22:34:30 +0000

    arrow

---
+++ 03_basics/arrow_function.js
@@ -0,0 +1,19 @@
+// const ayush  = function (){
+//     let username = "ayus";
+//     console.log(this.username); //undefined
+// }
+
+// ayush();
+
+
+// const ayush  = function (){
+//     let username = "ayus";
+//     console.log(this); //  Object [global] { ek global object de raha hai jisme bahut sari value hai}
+// }
+
+// ayush();
+
+const add2 = (num1, num2) =>{
+    return num1 + num2;
+}
+console.log(add2(6,9));   //15
