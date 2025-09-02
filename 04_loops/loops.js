9c70d8980e1ae35e693bb1e33cb3c989b6f7d41e
Ayush-sis0diya <xerbias2026@gmail.com>
2025-09-02 14:58:03 +0000

    loop

---
+++ 04_loops/loops.js
@@ -0,0 +1,29 @@
+// for
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// console.log(element);
+
+//continue
+
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+        
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// break 
+for (let i = 0; i <=10; i++) {
+    if(i==5){
+        
+        break;
+    }
+    console.log(i);
+}
